9b29c623c3cc1499a21a738cc9aceecd23ceb066
kuwahara <wprotein.dk@gmail.com>
2026-01-07 15:57:35 +0900

    origin任意

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3001;
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
   });
   app.useGlobalPipes(new ValidationPipe());
